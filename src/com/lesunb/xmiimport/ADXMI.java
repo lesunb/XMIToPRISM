ed4cb98b4a322a561a42b5a7a3c17a781c2a4df1
Pedro Lima <ppslima@gmail.com>
2017-01-16 09:18:05 -0200

    importing sdmetrics and naming the package

---
+++ src/com/lesunb/xmiimport/ADXMI.java
@@ -0,0 +1,17 @@
+package com.lesunb.xmiimport;
+
+public class ADXMI extends XMIDiagram {
+	
+	String decisionNodesIDs[];
+	String executableNodesIDs[];
+	String initialNodesIDs[];
+	String finalNodesIDs[];
+	String mergeNodesIDs[];
+	String controlFlowsIDs[];
+	
+	public ADXMI(String id, String name, String type) {
+		super(id, name, type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/com/lesunb/xmiimport/TestClass.java
+++ src/com/lesunb/xmiimport/TestClass.java
@@ -1,6 +1,13 @@
+package com.lesunb.xmiimport;
 
 public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
+		// file reader
+		
+		
+		// output from file
+		
 	}
 }
